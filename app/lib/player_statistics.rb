e9dbb3afd1980643b2283bc38a6675b9502611a4
recroot89 <recroot89@gmail.com>
2019-10-09 03:41:38 +0500

    fix player statistics module

--- app/lib/player_statistics.rb
+++ app/lib/player_statistics.rb
@@ -17,15 +17,16 @@ module PlayerStatistics
   end
 
   def get_stats_leaders_from_team(team, player_stats_type)
-    Stat.includes(:player, :game)
-        .select { |s| s.player.team_id == team.id }
-        .sort_by(&:"#{player_stats_type}")
-        .reverse
-        .first(5)
+    games = Game.where(player_id: team.players)
+
+    Stat.where(game_id: games)
+        .order("#{player_stats_type}": :desc)
+        .limit(5)
+        .includes(game: [:player])
         .map do |s|
           [
-            "#{player_stats_type.capitalize}: #{s.send(player_stats_type)}",
-            "Name: #{s.player.first_name} #{s.player.last_name}",
+            "#{player_stats_type.to_s.humanize}: #{s.send(player_stats_type)}",
+            "Name: #{s.game.player.first_name} #{s.game.player.last_name}",
             "Match ID: #{s.game.match_id}",
             "Match date: #{s.game.game_date}"
           ]
@@ -33,35 +34,33 @@ module PlayerStatistics
   end
 
   def get_stats_leaders_from_all_teams(player_stats_type)
-    Stat.includes(player: [:team]).includes(:game)
-        .sort_by(&:"#{player_stats_type}")
-        .reverse
-        .first(5)
+    Stat.order("#{player_stats_type}": :desc)
+        .limit(5)
+        .includes(game: [player: [:team]])
         .map do |s|
           [
-            "Team: #{s.player.team.name}",
-            "#{player_stats_type.capitalize}: #{s.send(player_stats_type)}",
-            "Name: #{s.player.first_name} #{s.player.last_name}",
+            "Team: #{s.game.player.team.name}",
+            "#{player_stats_type.to_s.humanize}: #{s.send(player_stats_type)}",
+            "Name: #{s.game.player.first_name} #{s.game.player.last_name}",
             "Match ID: #{s.game.match_id}",
             "Match date: #{s.game.game_date}"
           ]
         end
   end
 
-  private
+  # private
 
   def get_stats_for_the_match(player, match)
-    current_game = Game.find_by(match_id: match, player_id: player)
-    player.stats.find_by(game_id: current_game)
+    Game.find_by(match_id: match, player_id: player).stat
   end
 
   def get_last_games(player, games_count = 3)
+    # to keep the order of games
     player.games.order(game_date: :desc).limit(games_count).order(game_date: :asc)
   end
 
   def get_stats_for_last_games(player, games_count)
-    last_games_ids = get_last_games(player, games_count).map(&:id)
-    player.stats.select { |stat| last_games_ids.include?(stat.game_id) }
+    Stat.where(game_id: get_last_games(player, games_count))
   end
 
   def challenge_completed?(player_stats, challenge_type)
@@ -78,4 +77,10 @@ module PlayerStatistics
       raise 'Only theese challenges allowed: distance, pass_accuracy, hattrick, no_goal'
     end
   end
+
+  singleton_class.send(:private,
+                       :get_stats_for_the_match,
+                       :get_last_games,
+                       :get_stats_for_last_games,
+                       :challenge_completed?)
 end
